b3c694d522de5f97dc10f595c567a9502d3d056e
NelsonRodMar <contact@nelsonrodmar.fr>
2023-05-14 04:16:00 +0100

    lisbon.txt

--- test/SelfySnapshot.ts
+++ test/SelfySnapshot.ts
@@ -7,6 +7,9 @@ describe("Selfy Test", function () {
   async function deployFixture() {
     const [owner, addr1] = await ethers.getSigners();
     const GHO_TOKEN_ADDRESS = "0xcbE9771eD31e761b744D3cB9eF78A1f32DD99211";
+    const GHO_PRICE = ethers.utils.parseEther("50");
+    const ETH_PRICE = ethers.utils.parseEther("0.1");
+    const baseURI = 'https://noun-api.com/beta/pfp';
 
     // Deploy Contract SelfyProfile with Parameters
     const SelfySnapshotGHO = await ethers.getContractFactory("SelfySnapshotGHO");
@@ -16,27 +19,32 @@ describe("Selfy Test", function () {
     const SelfySnapshotETH = await ethers.getContractFactory("SelfySnapshotETH");
     const selfySnapshotETH = await SelfySnapshotETH.deploy();
 
-    return { selfySnapshotETH, selfySnapshotGHO,  owner, addr1, GHO_TOKEN_ADDRESS};
+    return { selfySnapshotETH, selfySnapshotGHO,  owner, addr1, GHO_TOKEN_ADDRESS, GHO_PRICE, ETH_PRICE, baseURI};
   };
 
   describe("SelfySnpashot GHO and ETH", function () {
     it("Should return the right data", async function () {
-      const {selfySnapshotETH, selfySnapshotGHO,GHO_TOKEN_ADDRESS } = await loadFixture(deployFixture);
+      const {selfySnapshotETH, selfySnapshotGHO,GHO_TOKEN_ADDRESS, owner } = await loadFixture(deployFixture);
+      // GHO Deployement
       expect(await selfySnapshotGHO.ghoToken()).to.equal(GHO_TOKEN_ADDRESS);
+      expect(await selfySnapshotGHO.hasRole(selfySnapshotGHO.DEFAULT_ADMIN_ROLE(), owner.address)).to.be.true;
+      // ETH Deployement
+      expect(await selfySnapshotETH.hasRole(selfySnapshotETH.DEFAULT_ADMIN_ROLE(), owner.address)).to.be.true;
+      expect(await selfySnapshotETH.hasRole(selfySnapshotETH.MINTER_ROLE(), owner.address)).to.be.true;
     });
-/*
-    it("Should mint a SelfyProfile", async function () {
-        const {selfyProfile, baseURI, defaultURIValue, addr1, selfyBadge, owner} = await loadFixture(deployFixture);
-        await selfyProfile.connect(addr1).createSelfyProfile();
-        expect(await selfyProfile.balanceOf(addr1.address)).to.equal(1);
-        expect(await selfyProfile.tokenURI(0)).to.equal(
-            baseURI
-            +  defaultURIValue.head
-            +"&background=" + defaultURIValue.background
-            +"&body=" + defaultURIValue.body
-            +"&accessory=" + defaultURIValue.accessory
-            +"&glasses=" + defaultURIValue.glasses
-        );
-    });*/
+
+    it("Should mint a SelfyProfile w/ ETH", async function () {
+        const {selfySnapshotETH, baseURI, addr1, ETH_PRICE } = await loadFixture(deployFixture);
+
+        // Mint the SnapShot NFToken
+        await selfySnapshotETH.connect(addr1).mint(baseURI, {value: ETH_PRICE});
+        expect(await selfySnapshotETH.balanceOf(addr1.address)).to.equal(1);
+        expect(await selfySnapshotETH.tokenURI(1)).to.equal(baseURI);
+    });
+
+    it("Should mint a SelfyProfile w/ GHO", async function () {
+        const {owner, selfySnapshotETH, baseURI, addr1, GHO_PRICE, gho } = await loadFixture(deployFixture);
+
+    });
   })
 });
